d520f6277cbf783b7887cbd40df0410e815297e7
justinbrooks2552 <justinbrooks2552@gmail.com>
2023-12-21 12:25:27 -0600

    test

---
+++ tpa.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+
+
+st.markdown('# test')
