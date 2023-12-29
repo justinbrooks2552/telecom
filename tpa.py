f3fa96d84287670c6f2fc988131acff280e193f4
justinbrooks2552 <justinbrooks2552@gmail.com>
2023-12-29 12:16:21 -0600

    update

--- tpa.py
+++ tpa.py
@@ -6,7 +6,7 @@ import streamlit as st
 # Imports specific functionalities from scikit-learn needed for encoding data, splitting it, training a model, and evaluating it.
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 # Loads the telecom dataset into a DataFrame called 't'.
@@ -14,13 +14,11 @@ t = pd.read_csv("telecomdataset.csv")
 
 
 
-# Drops any rows with missing values to ensure the quality of the data for analysis.
-t= t.dropna()
 
 # Initializes a dictionary to hold state abbreviations to unique number mappings.
 state_to_number = {}
 
-state_abbreviations = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+state_abbreviations = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC","DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
 # Maps each state abbreviation to a unique number starting from 1.
 for i, state_abbreviation in enumerate(state_abbreviations):
@@ -57,6 +55,9 @@ t['churn1'] = np.where(t['churn'] == 1, 1, 0)
 
 # Removes the original 'churn' column after encoding it.
 t = t.drop(columns=['churn'])
+
+# Drops any rows with missing values to ensure the quality of the data for analysis.
+t= t.dropna()
 
 state_abbreviation_to_convert = "AZ"  # Replace with the state abbreviation you want to convert
 if state_abbreviation_to_convert in state_to_number:
@@ -112,7 +113,7 @@ st.markdown(
 
 
 # List of state abbreviations
-state_abbreviations = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+state_abbreviations = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
                        "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
                        "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
                        "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
@@ -120,22 +121,8 @@ state_abbreviations = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA
 
 
 
-# # Increase font size with HTML and inline CSS
-# st.markdown("""
-#     <style>
-#     .big-font {
-#         font-size:16px !important;
-#     }
-#     </style>
-#     <div class='big-font'>
-#         Select Customer State
-#     </div>
-#     """, unsafe_allow_html=True)
-
-# # Rest of your Streamlit app code
 
 
-# Increase font size and make text bold with HTML and inline CSS
 st.markdown("""
     <style>
     .big-and-bold-font {
@@ -148,22 +135,16 @@ st.markdown("""
     </div>
     """, unsafe_allow_html=True)
 
-# Rest of your Streamlit app code
-
-
-# def main():
-#     st.title('Select Your State Abbreviation')
 
 stateoption = st.selectbox(
     '',
-        state_abbreviations, 
-        index=None, 
+        state_abbreviations,  
         placeholder="Select a state abbreviation..."
     )
 
 st.write('***Customer lives in:***', stateoption)
 
-# If-else statements for each state abbreviation
+
 if stateoption == 'AL':
     stateoption = 1
 elif stateoption == 'AK':
@@ -178,114 +159,121 @@ elif stateoption == 'CO':
     stateoption = 6
 elif stateoption == 'CT':
     stateoption = 7
-elif stateoption == 'DE':
+elif stateoption == 'DC':
     stateoption = 8
-elif stateoption == 'FL':
+elif stateoption == 'DE':
     stateoption = 9
-elif stateoption == 'GA':
+elif stateoption == 'FL':
     stateoption = 10
-elif stateoption == 'HI':
+elif stateoption == 'GA':
     stateoption = 11
-elif stateoption == 'ID':
+elif stateoption == 'HI':
     stateoption = 12
-elif stateoption == 'IL':
+elif stateoption == 'ID':
     stateoption = 13
-elif stateoption == 'IN':
+elif stateoption == 'IL':
     stateoption = 14
-elif stateoption == 'IA':
+elif stateoption == 'IN':
     stateoption = 15
-elif stateoption == 'KS':
+elif stateoption == 'IA':
     stateoption = 16
-elif stateoption == 'KY':
+elif stateoption == 'KS':
     stateoption = 17
-elif stateoption == 'LA':
+elif stateoption == 'KY':
     stateoption = 18
-elif stateoption == 'ME':
+elif stateoption == 'LA':
     stateoption = 19
-elif stateoption == 'MD':
+elif stateoption == 'ME':
     stateoption = 20
-elif stateoption == 'MA':
+elif stateoption == 'MD':
     stateoption = 21
-elif stateoption == 'MI':
+elif stateoption == 'MA':
     stateoption = 22
-elif stateoption == 'MN':
+elif stateoption == 'MI':
     stateoption = 23
-elif stateoption == 'MS':
+elif stateoption == 'MN':
     stateoption = 24
-elif stateoption == 'MO':
+elif stateoption == 'MS':
     stateoption = 25
-elif stateoption == 'MT':
+elif stateoption == 'MO':
     stateoption = 26
-elif stateoption == 'NE':
+elif stateoption == 'MT':
     stateoption = 27
-elif stateoption == 'NV':
+elif stateoption == 'NE':
     stateoption = 28
-elif stateoption == 'NH':
+elif stateoption == 'NV':
     stateoption = 29
-elif stateoption == 'NJ':
+elif stateoption == 'NH':
     stateoption = 30
-elif stateoption == 'NM':
+elif stateoption == 'NJ':
     stateoption = 31
-elif stateoption == 'NY':
+elif stateoption == 'NM':
     stateoption = 32
-elif stateoption == 'NC':
+elif stateoption == 'NY':
     stateoption = 33
-elif stateoption == 'ND':
+elif stateoption == 'NC':
     stateoption = 34
-elif stateoption == 'OH':
+elif stateoption == 'ND':
     stateoption = 35
-elif stateoption == 'OK':
+elif stateoption == 'OH':
     stateoption = 36
-elif stateoption == 'OR':
+elif stateoption == 'OK':
     stateoption = 37
-elif stateoption == 'PA':
+elif stateoption == 'OR':
     stateoption = 38
-elif stateoption == 'RI':
+elif stateoption == 'PA':
     stateoption = 39
-elif stateoption == 'SC':
+elif stateoption == 'RI':
     stateoption = 40
-elif stateoption == 'SD':
+elif stateoption == 'SC':
     stateoption = 41
-elif stateoption == 'TN':
+elif stateoption == 'SD':
     stateoption = 42
-elif stateoption == 'TX':
+elif stateoption == 'TN':
     stateoption = 43
-elif stateoption == 'UT':
+elif stateoption == 'TX':
     stateoption = 44
-elif stateoption == 'VT':
+elif stateoption == 'UT':
     stateoption = 45
-elif stateoption == 'VA':
+elif stateoption == 'VT':
     stateoption = 46
-elif stateoption == 'WA':
+elif stateoption == 'VA':
     stateoption = 47
-elif stateoption == 'WV':
+elif stateoption == 'WA':
     stateoption = 48
-elif stateoption == 'WI':
+elif stateoption == 'WV':
     stateoption = 49
-elif stateoption == 'WY':
+elif stateoption == 'WI':
     stateoption = 50
+elif stateoption == 'WY':
+    stateoption = 51
 else:
     st.write('') #**Please select a state abbreviation from the list.** Optional formatting text
 
 st.divider()
 
-accountlength = st.number_input("How long have they been a customer?", value=None, placeholder="Type a number...")
-st.write('**Loyal Customer for**', accountlength, '**months**')
+
+accountlength = st.number_input(
+    'How long have they been a customer?', min_value=1, max_value=360, step= 1, format=None)
+st.write('***Loyal Customer for*** ', accountlength, "***months***")
+
+# accountlength = st.number_input("How long have they been a customer?", value=1, placeholder="Type a number...")
+# st.write('**Loyal Customer for**', accountlength, '**months**')
 
 
-# Get unique 'accountlength' values and sort them
-unique_account_lengths = sorted(t['accountlength'].unique())
+# # Get unique 'accountlength' values and sort them
+# unique_account_lengths = sorted(t['accountlength'].unique())
 
 
-# Looping through each possible account length
-for length in unique_account_lengths:
-    if accountlength == length:
-       # st.write(f"You have selected an account length of {length}")
-        break  # Exit the loop once the right account length is found
+# # Looping through each possible account length
+# for length in unique_account_lengths:
+#     if accountlength == length:
+#        # st.write(f"You have selected an account length of {length}")
+#         break  # Exit the loop once the right account length is found
 
 
 areacode = st.selectbox("Select Area Code Below", 
-                        ('408', '415', '510'), index=None, placeholder="Select Area Code Below...")
+                        ('408', '415', '510'), placeholder="Select Area Code Below...")
 st.write('**Customer Area Code is**', areacode)
 
 if areacode == 408:
@@ -326,33 +314,6 @@ else:
     voicemailplan= 0
 
 
-
-# numbervmailmessages = st.number_input("How many voicemail messages does the customer have?", value=None, placeholder="Type a number...")
-# st.write('**Customer has**', numbervmailmessages, '**voicemail messages**')
-
-
-# # Get unique 'accountlength' values and sort them
-# unique_number_vmail_messages = sorted(t['numbervmailmessages'].unique())
-
-
-# # Looping through each possible vmails
-# for vmail in unique_number_vmail_messages:
-#     if numbervmailmessages == vmail:
-#         break  # Exit the loop once the right account length is found
-         
-
-# # totaldayminutes = st.number_input('Total Day Minutes', min_value=0.0, max_value= 240.0, step=0.1)
-
-# # st.write('Total Day Minutes Used:', totaldayminutes)
-
-
-# totaldayminutes = st.number_input('Total Day Minutes', min_value=0.0, max_value=240.0, step=0.1)
-
-# # Round the input to 1 decimal place
-# totaldayminutes_rounded = round(totaldayminutes, 1)
-
-# # Display the rounded value
-# st.write(f'Total Day Minutes: {totaldayminutes_rounded}')
 
 
 numbervmailmessages = st.number_input(
@@ -425,7 +386,7 @@ st.write(f'Total International Charge: ${totalintercharges_rounded}')
 customerservicecalls = st.selectbox("Select Number of Customer Service Calls?",
                         ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
                          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'),
-                        index=None, 
+                        index=0, 
                         placeholder="Select Number of Customer Service Calls Made by Customer...")
 
 st.write('**Customer Service Calls Made:**', customerservicecalls)
@@ -454,10 +415,14 @@ newdata = pd.DataFrame({
     "totalintercharge" : [totalintercharge],
     "customerservicecalls" : [customerservicecalls]      
 })
-newdata['churn']=clf.predict(newdata)
 
-if st.button('Determine Churn', key='churn', type='primary'):
-    prediction = newdata['churn']
+
+
+newdata["predictcustomerchurn"] = clf.predict(newdata)
+
+
+if st.button('Determine Churn', key='predictcustomerchurn', type='primary'):
+    prediction = newdata['predictcustomerchurn']
     if any(prediction == 1):
         st.write('***Start Customer Retention Workflow***')
     else:
